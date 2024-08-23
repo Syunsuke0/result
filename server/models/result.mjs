9d1c1af256eaf348d7060dc60732bcd85b1a69a4
Syunsuke Shima <s.syun617@icloud.com>
2024-08-23 23:10:23 +0900

    🔧 型を定義

---
+++ server/models/result.mjs
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const resultSchema = Schema({
+  match_result: {
+    type: String,
+    enum: ["win", "lose", "draw"],
+    required: true,
+  },
+  character_used: {
+    type: String,
+    enum: ["女王", "破輪", "魔女"],
+    required: true,
+  },
+  map: {
+    type: String,
+    required: true,
+    enum: [
+      "軍需工場",
+      "赤の教会",
+      "精神病院",
+      "湖景村",
+      "月の河公園",
+      "レオの思い出",
+      "永眠町",
+      "中華街",
+    ],
+  },
+});
+
+const Result = model("Result", resultSchema);
+export default Result;
