86325bec096c57649e940383fe4adc5730754f5f
Syunsuke Shima <s.syun617@icloud.com>
2024-09-28 11:19:37 +0900

    ✨ Pickerコンポーネント

---
+++ components/CharaPicker.jsx
@@ -0,0 +1,14 @@
+const CharaPicker = () => {
+  return (
+    <label>
+      Pick a used Character :
+      <select name="selectedFruit">
+        <option value="女王">女王</option>
+        <option value="破輪">破輪</option>
+        <option value="魔女">魔女</option>
+      </select>
+    </label>
+  );
+};
+
+export default CharaPicker;
---
+++ components/MapPicker.jsx
@@ -0,0 +1,19 @@
+const MapPicker = () => {
+  return (
+    <label>
+      Pick a used Map :
+      <select name="selectedFruit">
+        <option value="軍需工場">軍需工場</option>
+        <option value="赤の教会">赤の教会</option>
+        <option value="聖心病院">聖心病院</option>
+        <option value="湖景村">湖景村</option>
+        <option value="月の河公園">月の河公園</option>
+        <option value="レオの思い出">レオの思い出</option>
+        <option value="永眠町">永眠町</option>
+        <option value="中華街">中華街</option>
+      </select>
+    </label>
+  );
+};
+
+export default MapPicker;
