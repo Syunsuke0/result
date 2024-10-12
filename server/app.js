09ea19467ae7ca69c63c672f757d1e9a4fe17ad2
Syunsuke Shima <s.syun617@icloud.com>
2024-10-12 18:50:35 +0900

    🔥

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const port = 8080;
+const apiRoutes = require("./server/api-routes");
+const { connect, Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 8080;
 const cors = require("cors");
+
+const env = require("dotenv");
+env.config();
+
+//API
+app.use("/api", apiRoutes);
 
 //JSONデータのパースを有効化
 app.use(bodyParser.json());
@@ -17,11 +26,44 @@ app.use(
   })
 );
 
-app.post("/api/results", (req, res) => {
-  const formData = req.body;
-  console.log(formData);
-  res.send("フォームデータを受け取りました");
-});
+//mongooseとの接続
+connect(process.env.MONGO_URI);
+
+const resultSchema = new Schema(
+  {
+    usedCharacter: {
+      type: String,
+      enum: ["女王", "破輪", "魔女", "ガラテア"],
+      required: true,
+    },
+    usedMap: {
+      type: String,
+      enum: [
+        "軍需工場",
+        "赤の教会",
+        "聖心病院",
+        "湖景村",
+        "月の河公園",
+        "レオの思い出",
+        "永眠町",
+        "中華街",
+      ],
+      required: true,
+    },
+    result: {
+      type: String,
+      enum: ["win", "draw", "lose"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Result = model("Result", resultSchema);
+
+app.get("/api/results", async (req, res) => {});
+
+app.post("/api/results", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`ポート${port}でリクエストを受付中...`);
