1239c0e74ba88e96332ebd13aa12c1ee313fbffa
Syunsuke Shima <s.syun617@icloud.com>
2024-10-17 23:49:10 +0900

    🚚

--- server/app.mjs
+++ server/app.mjs
@@ -1,8 +1,7 @@
 import express from "express";
 const app = express();
 import apiRoutes from "./api-routes/index.mjs";
-import { connect, Schema, model } from "mongoose";
-import mongoose from "mongoose";
+import "./helpers/db.mjs";
 import bodyParser from "body-parser";
 const port = process.env.PORT || 8080;
 import cors from "cors";
@@ -25,45 +24,6 @@ app.use(
     credentials: true,
   })
 );
-
-//mongooseとの接続
-connect(process.env.MONGO_URI);
-
-const resultSchema = new Schema(
-  {
-    usedCharacter: {
-      type: String,
-      enum: ["女王", "破輪", "魔女", "ガラテア"],
-      required: true,
-    },
-    usedMap: {
-      type: String,
-      enum: [
-        "軍需工場",
-        "赤の教会",
-        "聖心病院",
-        "湖景村",
-        "月の河公園",
-        "レオの思い出",
-        "永眠町",
-        "中華街",
-      ],
-      required: true,
-    },
-    result: {
-      type: String,
-      enum: ["win", "draw", "lose"],
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
-
-const Result = model("Result", resultSchema);
-
-app.get("/api/results", async (req, res) => {});
-
-app.post("/api/results", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`ポート${port}でリクエストを受付中...`);
