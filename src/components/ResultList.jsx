6657ec6124d67fd6b08ce93be51aaca73b5b041a
Syunsuke Shima <s.syun617@icloud.com>
2024-10-22 15:55:20 +0900

    💩 🚧 試合結果を表示したかった

--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -1,4 +1,7 @@
-const ResultList = () => {
+import { fetchResultsData } from "@/libs/fetchResultsData";
+
+const ResultList = (props) => {
+  const { results } = props.data;
   return (
     <div className="">
       <h3 className="text-center">戦績一覧</h3>
@@ -6,6 +9,17 @@ const ResultList = () => {
         <div className="bg-gray-200 p-2">試合結果</div>
         <div className="bg-gray-200 p-2">使用キャラ</div>
         <div className="bg-gray-200 p-2">使用マップ</div>
+        <div>
+          {results === undefined ? (
+            <p>Loading...</p> // resultsがundefinedの時
+          ) : (
+            results.map((result, index) => (
+              <div key={index}>
+                <p>{result.result}</p> // resultsが存在するとき
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
